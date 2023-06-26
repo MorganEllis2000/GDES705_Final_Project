2b6d7ad986ebb795c2a0d3c53842fb35c58f1db9
MohamadAlsamman <moealsamman@gmail.com>
2023-06-26 12:10:54 +0100

    Mansion Level Greybox + Changing Character BP
    Changed the position of the player camera, and started with the level greyboxing

--- Codename_Lost/Intermediate/Build/Win64/UnrealEditor/Inc/Codename_Lost/UHT/Gun.gen.cpp
+++ Codename_Lost/Intermediate/Build/Win64/UnrealEditor/Inc/Codename_Lost/UHT/Gun.gen.cpp
@@ -274,15 +274,15 @@ void EmptyLinkFunctionForGeneratedCodeGun() {}
 	}
 	DEFINE_VTABLE_PTR_HELPER_CTOR(AGun);
 	AGun::~AGun() {}
-	struct Z_CompiledInDeferFile_FID_Github_GDES705_Final_Project_Codename_Lost_Source_Codename_Lost_Gun_h_Statics
+	struct Z_CompiledInDeferFile_FID_Codename_Lost_Source_Codename_Lost_Gun_h_Statics
 	{
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
-	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Github_GDES705_Final_Project_Codename_Lost_Source_Codename_Lost_Gun_h_Statics::ClassInfo[] = {
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Codename_Lost_Source_Codename_Lost_Gun_h_Statics::ClassInfo[] = {
 		{ Z_Construct_UClass_AGun, AGun::StaticClass, TEXT("AGun"), &Z_Registration_Info_UClass_AGun, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AGun), 687412054U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Github_GDES705_Final_Project_Codename_Lost_Source_Codename_Lost_Gun_h_778971681(TEXT("/Script/Codename_Lost"),
-		Z_CompiledInDeferFile_FID_Github_GDES705_Final_Project_Codename_Lost_Source_Codename_Lost_Gun_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Github_GDES705_Final_Project_Codename_Lost_Source_Codename_Lost_Gun_h_Statics::ClassInfo),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Codename_Lost_Source_Codename_Lost_Gun_h_778971681(TEXT("/Script/Codename_Lost"),
+		Z_CompiledInDeferFile_FID_Codename_Lost_Source_Codename_Lost_Gun_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Codename_Lost_Source_Codename_Lost_Gun_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
